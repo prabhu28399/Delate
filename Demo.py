230fbd8463e0eeb18ebc6b3ad75618663aa946a4
prabhu28399 <prabhu28399@gmail.com>
2024-07-23 16:49:49 +0530

    one thing

--- Demo.py
+++ Demo.py
@@ -1,2 +1,8 @@
-print("this is for testing only");
-print("this is for testing only again printing");
+
+hungry = input("enter yes or no")
+if hungry == "yes":
+    print("eat banana")
+    print("eat apple")
+    print("eat carrot")
+else:
+    print("go and sleep")
